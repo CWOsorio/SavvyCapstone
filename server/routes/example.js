bd29db09165000143d0ccfed284e2b1dce0d4e3a
CWOsorio <63197754+CWOsorio@users.noreply.github.com>
2021-10-24 18:33:38 -0500

    Development (#19)
    * updated redirects order
    
    * added quotes/updated styling
    
    * updating parcel-build
    
    * updated parcel build
    
    * adding express server
    
    * adding css and mongo updates
    
    * adding MongoDb info
    
    * adding css updates

--- server/routes/example.js
+++ server/routes/example.js
@@ -17,4 +17,22 @@ router.route("/").get((request, response) => {
 //   });
 // });
 
+
+// Get all pizza records
+// router.get("/", (request, response) => {
+//   pizza.model.find({}, (error, data) => {
+//     if (error) return response.sendStatus(500).json(error);
+//     return response.json(data);
+//   });
+// });
+
+// Get a pizza by ID
+// router.get('/:id', (request, response) => {
+//   pizza.model.findById(request.params.id, (error, data) => {
+//     if (error) return response.sendStatus(500).json(error);
+//     return response.json(data);
+//   });
+// });
+=======
+
 // module.exports = router;
--- store/Gallery.js
+++ store/Gallery.js
@@ -1,4 +1,5 @@
 export default {
   header: "Handcrafted Hair Bows",
   view: "Gallery"
+
 };
