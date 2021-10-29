03f42a3d6b30eca2f80150d465d498a389f969a0
Cherisse Osorio <cherisseosorio@gmail.com>
2021-10-29 17:07:01 -0500

    updated audio

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,6 +1,6 @@
 import html from "html-literal";
 import ellaDrawing from '/assets/img/ellaDrawing.jpg';
-import hairUp from '/assets/music/hairUp.mp3';
+//import hairUp from '/assets/music/hairUp.mp3';
 
 const kelvinToFahrenheit = kelvinTemp =>
   Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
@@ -10,9 +10,10 @@ export default st => html`
 Weather in ${st.weather.city} ${kelvinToFahrenheit(st.weather.temp)} F, feels
 like ${kelvinToFahrenheit(st.weather.feelsLike)} F
 </h3>
-<audio controls>
+
+<!-- <audio autoplay>
 <source src="${hairUp}" type="audio/mpeg">
-</audio>
+</audio> -->
 
 <img src="${ellaDrawing}" />
 
