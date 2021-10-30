2903fbd06975947c7458b611172726df74a8f18e
Cherisse Osorio <cherisseosorio@gmail.com>
2021-10-30 10:27:17 -0500

    updating export vebiage

--- components/views/index.js
+++ components/views/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Bio } from "./Bio";
 export { default as Gallery } from "./Gallery";
-export { default as BowParty } from "./Bow Party";
+export { default as BowParty } from "./BowParty";
--- store/Bow Party.js
+++
@@ -1,4 +0,0 @@
-export default {
-  header: "Bow Party",
-  page: "Bow Party"
-};
